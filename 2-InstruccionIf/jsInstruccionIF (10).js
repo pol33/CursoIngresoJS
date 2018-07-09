22e221765403cafc4966c2cc3ce0b2545c699b71
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-09 19:59:31 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
 
+	var nota;
+
+	nota=Math.random()*10;
+	nota=nota.toFixed(0);
+
+	//alert(numeroRANDOM.toFixed(0));
+	if(nota<11&&nota>8){
+		alert("EXELENTE");
+	}
+	else if(nota<9&&nota>3){
+		  alert("aprobo");
+		}
+	
+	else{
+		alert("La proxima sera");
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numeroRANDOM;
+
+	numeroRANDOM=Math.random()*10;
+
+	alert(numeroRANDOM.toFixed(0));
+
 	
 
 }//FIN DE LA FUNCIÓN
