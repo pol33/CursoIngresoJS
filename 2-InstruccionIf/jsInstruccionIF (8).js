9c7935535919fb0c46a8395aaaa73f9977c5058d
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-09 19:24:44 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,20 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	var estadoCivil="Soltero";
 
-	
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
+		if(edad>18&&estadoCivil=="Soltero"){
+
+			alert("Es soltero y no es menor");
+		}
+		else{
+				alert("Nada");
+
+		}
 
 
 }//FIN DE LA FUNCIÓN
