a18a72792c5803f1cbcff07315388235c304873b
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-09 22:48:06 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
 
+	numeroUno=prompt("NumeroUno");
+	numeroDos=prompt("NumeroDos");
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos)
+
+	if("numeroUno"=="numeroDos"){
+		alert(numeroUno+nueroDos);
+	}
+	else{
+		suma=numeroUno+numerDos;
+	}
+
+	if(suma>10){
+		alert("La suma es "+suma+" y supero el 10.");
+	}
 }
