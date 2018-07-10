a87d826ae6ca7fb257902756adf0e5271319ea68
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-09 22:19:28 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	var Importe;
+	var Resultado;
 	
+	Importe=document.getElementById("importe").value;
+
+	Importe=parseInt(Importe);
+
+	Resultado=Importe*0.75;
+
+	document.getElementById("resultado").value=Resultado;
+		
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,15 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+	var Sueldo;
+	var Resultado;
+	
+	Sueldo=document.getElementById('sueldo').value;
+
+	Sueldo=parseInt(Sueldo);
+
+	Resultado=Sueldo*1.10;
+
+	document.getElementById('resultado').value=Resultado;
 	
 }
