1680b6aabbd7a533e31b1943e52bafcde4c08cf5
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-09 22:46:17 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+	var superficie;
 
+	ancho=prompt("Ingrese el ancho.");
+	largo=prompt("Ingrese el largo.");
+
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+
+	perimetro=largo*2+ancho*2;
+	superficie=largo*ancho;
+
+
+	alert("El perimetro es "+perimetro+" y la supérficie es "+superficie);
 }
