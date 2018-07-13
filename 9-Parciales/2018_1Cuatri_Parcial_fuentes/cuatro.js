3e12b64c482caa96d52d70dc22066f8060d82a6f
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-12 22:03:17 -0300

    Ejercicio4 Parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -4,22 +4,41 @@ function mostrar()
 	var numeroUno;
 	var numeroDos;
 	var suma;
+	var resta;
 
-	numeroUno=prompt("NumeroUno");
-	numeroDos=prompt("NumeroDos");
+
+	numeroUno=prompt("Ingrese un numero");
+	numeroDos=prompt("Ingrese otro numero");
 
 
 	if(numeroUno==numeroDos){
 		alert(numeroUno+numeroDos);
 	}
-	else{
-			numeroUno=parseInt(numeroUno);
-	        numeroDos=parseInt(numeroDos)
-			suma=numeroUno+numeroDos;
+	
+		numeroUno=parseInt(numeroUno);
+	    numeroDos=parseInt(numeroDos)
+
+	    if(numeroUno>numeroDos){
+
+	    	resta=numeroUno-numeroDos;
+	    	alert("La resta es: "+resta);
+
+	    }
+	    else{
+
+	    	suma=numeroUno+numeroDos;
+			//alert("La suma es: "+suma);
+	
+
+	  	 if(suma>10){
+			alert("La suma es "+suma+" y supero el valor de 10.");
+	    	}
+	   	 else{
+			alert("La suma es: "+suma);
+
 	}
 
-	if(suma>10){
-		alert("La suma es "+suma+" y supero el 10.");
-	}
+}
+			
 
 }
