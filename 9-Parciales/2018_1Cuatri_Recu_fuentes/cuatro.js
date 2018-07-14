f756f2a83e582ef1161b58d1253ec46370153381
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-13 22:11:49 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,48 @@
 function mostrar()
 {
 
+
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var division;
+
+
+	numeroUno=prompt("Ingrese un numero");
+	numeroDos=prompt("Ingrese otro numero");
+
+
+	if(numeroUno==numeroDos){
+		alert(numeroUno+numeroDos);
+	}
+
+	else{
+			numeroUno=parseInt(numeroUno);
+	    	numeroDos=parseInt(numeroDos)
+
+	        if(numeroUno>numeroDos){
+
+	    	division=numeroUno/numeroDos;
+	    	alert("La resta es: "+resta);
+
+	       }
+	          else{
+
+	    	  suma=numeroUno+numeroDos;
+			//alert("La suma es: "+suma);
+	
+
+	  	     if(suma<50){
+			 alert("La suma es "+suma+" y  es menor a 50.");
+	    	}
+
+	   	     else{
+			 alert("La suma es: "+suma);
+		   }
+		
+	}
+
+	}
+
+	
 }
