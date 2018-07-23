a2a7f9525e7e88809f4c276ae9607263b4621246
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-23 20:22:26 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,4 +1,48 @@
 function mostrar()
 {
+	var hora=document.getElementById("laHora").value;
+	hora=parseInt(hora);
+	var mensaje;
+	
+	switch(hora)
+	{
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		mensaje="La proxima sera";
+		break;
+		case 4:
+		case 5:
+		case 6:
+		mensaje="raspando";
+		if(hora<5){
+			mensaje=mensaje+" debes esforzarte mas";
+		}
+		break;
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		mensaje="Aprobo";
+		if(hora>8)
+		{
+			mensaje=mensaje+" muy bien";
+		
+			}
+		break;
+		default:
+		mensaje="Hora no valida";
+		}
+		alert(mensaje);
+	}
+				
+			
+				
+		
+		
+			 
+			
+			
 
-}
+
