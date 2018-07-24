8cf2fa9b6df2056b6e4442a54e99d4eb02e27fca
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-24 00:09:35 -0300

    Add files via upload

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,39 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var Piedra=0;
+var Papel=1;
+var Tijera=2;
 
 function comenzar()
 {
-	
-
+		eleccionMaquina=Math.random()*2;
+		eleccionMaquina=(eleccionMaquina.toFixed(0));
+		alert(eleccionMaquina);
+		
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	if (eleccionMaquina==Tijera)
+	 {
+	 	alert("Gano");
+	 }
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
+	if(eleccionMaquina==Piedra)
+	{
+		alert("Gano");
+	}
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	if(eleccionMaquina==Papel)
+	{
+		alert("Gano");
+	}	
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -2,30 +2,96 @@ var eleccionMaquina;
 var ContadorDeEmpates=0;
 var ContadorDeGanadas=0;
 var ContadorDePerdidas=0;
+var Piedra=0;
+var Papel=1;
+var Tijera=2;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 3
-	 	
+		eleccionMaquina=Math.random()*2;
+		eleccionMaquina=(eleccionMaquina.toFixed(0));
+		//alert(eleccionMaquina);
+			document.getElementById('ganadas').value=ContadorDeGanadas;
+	document.getElementById('empatadas').value=ContadorDeEmpates;
+	document.getElementById('perdidas').value=ContadorDePerdidas;
+
+		
 
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	if(eleccionMaquina==Papel)
+	{
+		alert("Gano");
+		ContadorDeGanadas++;
+	}
+	else
+	{
+		if (eleccionMaquina==Piedra)
+			 {
+			 	alert("Empato");
+			 	ContadorDeEmpates++;
+			 }
+			 else
+			 {
+			 	alert("Perdio");
+			 	ContadorDePerdidas++;
+			 }
+	}
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+		if(eleccionMaquina==Piedra)
+	{
+		alert("Gano");
+		ContadorDeGanadas++;
+	}
+	else
+	{
+		if (eleccionMaquina==Papel)
+			 {
+			 	alert("Empato");
+			 	ContadorDeEmpates++;
+			 }
+			 else
+			 {
+			 	alert("Perdio");
+			 	ContadorDePerdidas++;
+			 }
+	}
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
+		if(eleccionMaquina==Papel)
+	{
+		alert("Gano");
+		ContadorDeGanadas++;
+	}
+	else
+	{
+		if (eleccionMaquina==Tijera)
+			 {
+			 	alert("Empato");
+			 	ContadorDeEmpates++;
+			 }
+			 else
+			 {
+			 	alert("Perdio");
+			 	ContadorDePerdidas++;
+			 }
+	}
 	
 }//FIN DE LA FUNCIÓN
 
 function mostrarResultado()
 {
+	ContadorDeGanadas=document.getElementById('ganadas').value;
+	ContadorDeEmpates;
+	ContadorDePerdidas;      	
 
 }
