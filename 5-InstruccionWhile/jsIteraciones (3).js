b40ee5aa65b8e61282376ae912fcf224eecbc0c0
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-24 21:37:58 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+		var clave = prompt("ingrese el número clave.");
 
+		clave=prompt("Digite la clave");
+
+			while(cave!="utn750")
+			{
+				clave=prompt("Error digite otra vez");
+			}
+
+			alert("biren");
 
 }//FIN DE LA FUNCIÓN
