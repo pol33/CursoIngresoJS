f2f0ac9a8cc56e412c4a11269e9cd01d693e8470
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-24 21:57:58 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,15 +2,50 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
+	// declarar 
+	var maximo;
+	var minimo;
+	var numeroIngresado;
+	var bandera;
 	
 	var respuesta='si';
+	//maximo=-999999;
+	//minimo=999999;
+	bandera=0;
 
 	while(respuesta!='no')
 	{
-		
+			numeroIngresado=prompt("Ingrese un numero");
+			numeroIngresado=parseInt(numeroIngresado);		
+
+			contador++;
+			if(bandera==0)
+			{
+				maximo=numeroIngresado;
+				minimo=numeroIngresado;
+				bandera=1;
+			}
+			else{  
+					if(numeroIngresado>maximo)
+					{
+						maximo=numeroIngresado;
+					}
+			
+					if(numeroIngresado<minimo)
+					{
+						minimo=numeroIngresado;
+					}
+			
+				}
+			//contador++;
+
+
+
+		respuesta=prompt("Ingrese no para salir");
 	
 	}
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 
 
 
