31ffed01dc1b5263073db89de962dfea6db5a37a
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-27 22:13:50 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,21 +3,84 @@ function mostrar()
 
 	var contador=0;
 	//declarar contadores y variables
-	var positivos;
-	var negativos;
-	var ceros;
+	var positivo=0;
+	var negativo=0;
+	var ceros=0;
 	var diferencia;
-	var sumaPositivos=0; 
-	
+	var sumaPositivos=0;
+	var sumaNegativos=0;
+	var numerosPares=0;
+	var promedioPositivos;
+	var promedioNegativos;
 	var respuesta="si";
+	var numero;
 
 	while(respuesta!="no")
 	{
-		
-	
+		numero=prompt("Ingrese un numero");
+		numero=parseInt(numero);
+
+		while(numero==isNaN)
+		{
+			numero=prompt("Ingrese un numero");
+			numero=parseInt(numero);
+
+		}
+
+		//contador++;
+		if (numero<0) 
+			{
+				negativo=numero;
+				sumaNegativos+=numero;
+			}
+			else
+			{
+				if (numero==0)
+				 {
+				 	ceros=numero;
+				 }
+				 else
+				 	{
+				 		positivo=numero;
+				 		sumaPositivos+=numero;
+				 	}
+			}	
+
+		if (numero%2==0||numero==0)
+			 {
+			 	numerosPares=numero;
+			 }
+		if (negativo==0)
+			 {
+			 	negativo=1;
+			 }	
+		if (positivo==0)
+		 	{
+		 		positivo=1;
+		 	}
+		promedioPositivos=sumaPositivos/positivo;
+		promedioNegativos=sumaNegativos/negativo;
+
+		diferencia=positivo-negativo;
+
+
+		respuesta=prompt("Desea continuar");
+
+
+
+
+			
 	}
 
-	document.write("Hola");
+	document.write("1) "+sumaNegativos+"<br/>");
+	document.write("2) "+sumaPositivos+"<br/>");
+	document.write("3) "+positivo+"<br/>");
+	document.write("4) "+negativo+"<br/>");
+	document.write("5) "+ceros+"<br/>");
+	document.write("6) "+numerosPares+"<br/>");
+	document.write("7) "+promedioPositivos+"<br>");
+	document.write("8) "+promedioNegativos+"<br/>");
+	document.write("9) "+diferencia);
 
 
 
