0d04ab493c360e062f32f92432d53d2dd7835a70
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-30 21:19:26 -0300

    Add files via upload

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,62 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
-}
+	var edad;
+	var sexo;
+	var EstadoCivil;
+	var Sueldo;
+	var legajo;
+	var nacionalidad;
+
+	edad=prompt("Por favor ingrese su edad");
+	while((edad<18||edad>90)||isNaN(edad))
+		{
+			edad=prompt("Error ingrese una edad entre 18 y 90");
+		} 
+
+	sexo=prompt("Ingrese su sexo");
+	while(sexo!="F"&&sexo!="M")
+		{
+			sexo=prompt("Error ingrese"+"F"+"para femenino"+"y"+"M"+"para masculino");
+		}
+	EstadoCivil=prompt("Ingrese su estado civil.	Estado civil, 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos ");
+	EstadoCivil=parseInt(EstadoCivil);
+
+	while(EstadoCivil<1||EstadoCivil>4)
+		{
+			EstadoCivil=prompt("Error vuelva a ingresar un estado civil valido.");
+		}
+	Sueldo=prompt("Si tiene un sueldo ingrese el monto");
+	Sueldo=parseInt(Sueldo);
+	while(isNaN(Sueldo)||(Sueldo<0||Sueldo>10000000))
+		{
+			Sueldo=prompt("Error ingrese por lo menos $0.00");
+			Sueldo=parseInt(Sueldo);
+
+		}
+	legajo=prompt("Ingrese un numero telefonico");
+	legajo=parseInt(legajo);
+	while(isNaN(legajo)||legajo<9999999999)
+		{
+			legajo=prompt("Error ingrese el numero de area que es 54");
+			legajo=parseInt(legajo);
+		}	
+	nacionalidad=prompt("Ingrese su nacionalidad");
+
+	document.getElementById('Edad').value=edad;
+	document.getElementById('Sexo').value=sexo;
+	document.getElementById('EstadoCivil').value=EstadoCivil;
+	document.getElementById('Sueldo').value=Sueldo;
+	document.getElementById('Legajo').value=legajo;
+	document.getElementById('Nacionalidad').value=nacionalidad;
+
+
+
+
+
+
+
+
+
+
+}	
