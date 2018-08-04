912ac53c7d667eb655680cbd99ff6e1c55ebc220
pol33 <36174687+pol33@users.noreply.github.com>
2018-08-03 21:58:25 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,26 @@
 function mostrar()
 {
+		var personas;
+
+		var contador;
+		contador=0;
+
+		for( ;  ; )
+		{
+			contador++;
+
+			console.log(contador);
+			//contador++;
+			//break;
+
+			/*if (contador==5) 
+			{
+				break;
+			}*/
+		}
+
+			console.log("Final "+contador);
+
+
 
 }
