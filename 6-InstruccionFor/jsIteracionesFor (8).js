5b157b0f90df27b25b9d42568f1a8064da1e1814
pol33 <36174687+pol33@users.noreply.github.com>
2018-08-03 21:57:49 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,6 +1,37 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var numeroDivisor;
+	var numeroRecorrido;
+	var sumaNumerosDivisores=0;
+	var respuesta;
 
+	numeroIngresado=prompt("Ingrese numero");
+	numeroIngresado=parseInt(numeroIngresado);
+
+
+	 for( numeroRecorrido=1;numeroRecorrido<numeroIngresado ;numeroRecorrido++ )
+	 		{
+	 			if (numeroIngresado%numeroRecorrido==0)
+	 			 {
+	 			 	
+	 			 	numeroDivisor=numeroRecorrido;
+	 			 	sumaNumerosDivisores+=numeroDivisor;
+	 			 }
+	 		}
+
+
+	 		if (sumaNumerosDivisores==numeroIngresado)
+	 		 {
+	 		 	respuesta="El numero ingresado "+numeroIngresado+" es perfecto.";
+	 		 }
+	 		 else
+	 		 		{
+	 		 			respuesta="El numero ingresado "+numeroIngresado+" no es perfecto.";
+	 		 			
+	 		 		}
+
+	 		 alert(respuesta);
 
 
 
